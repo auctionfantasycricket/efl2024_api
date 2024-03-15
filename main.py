3f85407534b1bce6d6d4650563bc84210daa3d7f
Sakshar Thakkar <saksharhere@gmail.com>
2024-03-14 18:15:00 -0700

    updateplayer

--- main.py
+++ main.py
@@ -101,6 +101,67 @@ def get_player():
         return json.dumps({"message": "All players are processed"}), 404
 
 
+@app.route('/updateplayer/<_id>', methods=['PUT'])
+def update_player(_id):
+    updated_data = request.get_json()
+    filter = {"_id": ObjectId(str(_id))}
+    # Get the collectionName from the query parameter
+    collection_name = request.args.get(
+        'collectionName', 'efl_playersCentral_test')
+
+    collections = db[collection_name]
+    # Get the collectionName from the query parameter
+
+    # Exclude _id from update_data to avoid updating it
+    updated_data.pop('_id', None)
+    result = collections.update_one(filter, {"$set": updated_data})
+
+    if updated_data['status'] == "sold":
+        ownercollection = request.args.get(
+            'ownerCollectionName', 'efl_ownerTeams_test')
+
+        ownercollection = db[ownercollection]
+        update_owner_data(updated_data, ownercollection)
+
+    return json_util.dumps(result.raw_result)
+
+
+def update_owner_data(updated_data, ownercollection):
+    owner_team = updated_data['ownerTeam']
+    myquery = {"teamName": owner_team}
+    owners_data = ownercollection.find(myquery)
+
+    for owner_items in owners_data:
+        owner_items = update_owner_items(owner_items, updated_data)
+        filter_owner = {"_id": ObjectId(str(owner_items["_id"]))}
+        ownercollection.update_one(filter_owner, {"$set": owner_items})
+
+
+def update_owner_items(owner_items, updated_data):
+    owner_items["currentPurse"] -= int(updated_data["boughtFor"])
+    owner_items["totalCount"] += 1
+    owner_items["maxBid"] = owner_items["currentPurse"] - \
+        (20 * (15 - owner_items["totalCount"]))
+
+    role = updated_data["player_role"]
+    if role == "Batter":
+        owner_items["batCount"] += 1
+    elif role == "Bowler":
+        owner_items["ballCount"] += 1
+    elif role == "All-Rounder":
+        owner_items["arCount"] += 1
+    elif role == "WK Kepper - Batter":
+        owner_items["batCount"] += 1
+        owner_items["wkCount"] += 1
+    else:
+        print("Role not found")
+
+    if updated_data["country"] != "India":
+        owner_items["fCount"] += 1
+
+    return owner_items
+
+
 if __name__ == '__main__':
     # Run the Flask app on http://127.0.0.1:5000/
     app.run()
