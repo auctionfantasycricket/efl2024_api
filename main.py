1d2ca4ea6382f645e70af0a92cbaa6b8201d0c5d
Sakshar Thakkar <saksharhere@gmail.com>
2025-04-06 12:46:59 -0700

    transfers_bp

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from draftapi import draftapi_bp  # Import the Blueprint
 from liveupdates import liveupdates_bp
 import logging
 import jwt
+from transfers import transfers_bp
 
 
 @app.route('/sample_api', methods=['GET'])
@@ -1333,6 +1334,7 @@ def eod_update():
 
 app.register_blueprint(draftapi_bp)
 app.register_blueprint(liveupdates_bp)
+app.register_blueprint(transfers_bp)
 
 if __name__ == '__main__':
     # Run the Flask app on http://127.0.0.1:5000/
